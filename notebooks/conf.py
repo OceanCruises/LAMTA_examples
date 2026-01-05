168b047f4e056de2c413bb2c283decf9450c33a3
Lloyd Izard <76954858+LOCEANlloydizard@users.noreply.github.com>
2026-01-05 06:33:33 -0400

    fix RTD keyboard interupt

--- notebooks/conf.py
+++ notebooks/conf.py
@@ -2,22 +2,13 @@
 project = "LAMTA Examples"
 author = "OceanCruises"
 
-extensions = [
-    "myst_nb",
-    "sphinx_book_theme",
-    "sphinx_external_toc",
-    "sphinx_design",
-]
-
 master_doc = "index"
 
-# Do not execute notebooks during docs build (render stored outputs)
+extensions = ["myst_nb", "sphinx_book_theme", "sphinx_external_toc", "sphinx_design"]
+
+# myst-nb: do not execute, render stored outputs
 nb_execution_mode = "off"
 nb_execution_timeout = -1
-
-# (Optional compatibility: keep these too; harmless if unused)
-mystnb_execution_mode = "off"
-mystnb_execution_timeout = -1
 
 source_suffix = [".md", ".rst", ".ipynb"]
 
