56d933624ce7973d4740c58c842784c6c1e40be3
Lloyd Izard <76954858+LOCEANlloydizard@users.noreply.github.com>
2026-01-11 17:51:19 -0800

    fix RTD

--- notebooks/conf.py
+++ notebooks/conf.py
@@ -1,4 +1,7 @@
 # notebooks/conf.py
+from pathlib import Path
+import yaml
+
 project = "LAMTA Examples"
 author = "OceanCruises"
 
@@ -8,13 +11,11 @@ extensions = [
     "myst_nb",
     "sphinx_book_theme",
     "sphinx_external_toc",
-    "sphinx_design"
+    "sphinx_design",
+    "sphinx_jinja",
 ]
 
-myst_enable_extensions = [
-    "colon_fence",
-]
-
+myst_enable_extensions = ["colon_fence"]
 
 # myst-nb: do not execute, render stored outputs
 nb_execution_mode = "off"
@@ -24,3 +25,21 @@ source_suffix = [".md", ".rst", ".ipynb"]
 
 html_theme = "sphinx_book_theme"
 external_toc_path = "_toc.yml"
+
+# Jinja gallery support
+HERE = Path(__file__).parent
+gallery_path = HERE / "gallery.yml"
+
+gallery_data = []
+if gallery_path.exists():
+    gallery_data = yaml.safe_load(gallery_path.read_text(encoding="utf-8")) or []
+
+# Tell sphinx-jinja which source files are Jinja templates
+jinja_templates = ["notebooks_overview.md"]
+
+# Provide variables accessible inside the template
+jinja_contexts = {
+    "notebooks_overview": {
+        "data": gallery_data,
+    }
+}
--- notebooks/notebooks_overview.md
+++ notebooks/notebooks_overview.md
@@ -3,18 +3,15 @@
 ::::{grid} 1 2 2 3
 :gutter: 2
 
-{% set data = yaml_load_file("gallery.yml") %}
 {% for item in data %}
-
 :::{card}
 :link: {{ item.website or item.notebook }}
 {% if item.image %}:img-top: {{ item.image }}{% endif %}
 
 **{{ item.name }}**
 
-{{ item.summary }}
-
+{% if item.summary %}{{ item.summary }}{% endif %}
 :::
-
 {% endfor %}
+
 ::::
